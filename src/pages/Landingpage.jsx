cea71b22904295147c7c6def4dde7cc6c4c6ab3c
Akshayak8 <srigadaakshay@gmail.com>
2024-04-17 12:47:52 +0530

    new

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from "../components/Main";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../components/Login";
+const Landingpage = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default Landingpage;
