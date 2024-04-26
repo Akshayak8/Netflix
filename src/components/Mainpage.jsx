eb5946a5c9281e8b07918d136958bd53c2b3fe1a
Akshayak8 <srigadaakshay@gmail.com>
2024-04-26 21:52:32 +0530

    neww

---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Mainpage = () => {
+  return (
+    <div>
+      <Link to="/Main">
+        <a href="">Join</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Mainpage;
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Main from "../components/Main";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../components/Login";
+import Mainpage from "../components/Mainpage";
 const Landingpage = () => {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/" element={<Mainpage />} />
           <Route path="/Main" element={<Main />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
